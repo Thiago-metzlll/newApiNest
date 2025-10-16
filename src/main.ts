63e4641bb7a3d18b8f9c6cec81dc48564777fcde
Thiago-metzlll <thiagostopassola@gmail.com>
2025-10-16 18:34:52 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); // <--- Adicione esta linha
 
   // ValidationPipe habilita validação via DTO (class-validator)
   // transform: true -> converte payloads (strings -> numbers) quando possível
@@ -16,6 +17,7 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+  
 
   await app.listen(3000);
   console.log('Server running on http://localhost:3000');
