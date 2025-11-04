0d657a67b05b9aacebd8ac188785550eb5f578f7
Thiago-metzlll <thiagostopassola@gmail.com>
2025-11-04 17:56:04 -0300

    att

--- src/user/model/bancoDadosUser.ts
+++ src/user/model/bancoDadosUser.ts
@@ -1,4 +1,12 @@
 // src/users/users.data.ts
-export const users = [
+export const users = [{id: 1,
+  name: "Thiago Souza",
+  email: "thiago@example.com",
+  password: "123456",
+  city: "Curitiba",
+  yearsOld: 23,
+  descriptionUser: "Desenvolvedor full stack apaixonado por tecnologia.",
+  preferences: "Frontend, UI/UX, React",
+  socialMedia: "@thiago.dev"}
   
 ];
