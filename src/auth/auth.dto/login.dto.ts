be9564d867cd30ebe5ede46eab2e8f8264883dfb
Thiago-metzlll <thiagostopassola@gmail.com>
2025-11-13 15:55:31 -0300

    att

--- src/auth/auth.dto/login.dto.ts
+++ src/auth/auth.dto/login.dto.ts
@@ -3,9 +3,9 @@ import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class LoginDto {
   @IsEmail({}, { message: 'O email deve ser válido.' })
-  email: string;
+  email!: string;  // adicionado o !
 
   @IsString()
   @MinLength(6, { message: 'A senha deve ter no mínimo 6 caracteres.' })
-  password: string;
+  password!: string;  // adicionado o !
 }
--- src/user/dto/createUser.dto.ts
+++ src/user/dto/createUser.dto.ts
@@ -12,22 +12,22 @@ import {
 export class CreateUserDto {
   @IsString()
   @IsNotEmpty({ message: 'O nome é obrigatório.' })
-  name: string;
+  name!: string;
 
   @IsEmail({}, { message: 'O email deve ser válido.' })
-  email: string;
+  email!: string;
 
   @IsString()
   @MinLength(6, { message: 'A senha deve ter no mínimo 6 caracteres.' })
-  password: string;
+  password!: string;
 
   @IsString()
   @IsNotEmpty({ message: 'A cidade é obrigatória.' })
-  city: string;
+  city!: string;
 
   @IsNumber({}, { message: 'A idade deve ser um número.' })
   @Min(0, { message: 'A idade não pode ser negativa.' })
-  yearsOld: number;
+  yearsOld!: number;
 
   @IsOptional()
   @IsString({ message: 'A descrição deve ser uma string.' })
