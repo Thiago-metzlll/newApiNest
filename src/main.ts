42cf51a485d55ad718fcc621df5faa1767a24ba5
Seu Nome <thiagostopassola@gmail.com>
2026-02-11 19:41:25 -0300

    fix: habilitando cors

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupApp } from './setup';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',
+      'https://ecommerce-front-55cr.vercel.app/',
+    ],
+    credentials: true,
+  });
 
   setupApp(app);
 
   await app.listen(3000);
   console.log('🚀 Server running on http://localhost:3000');
 }
+
 bootstrap();
